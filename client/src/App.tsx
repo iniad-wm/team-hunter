9e3dd99bbf072ba2437d1bb500689b74718e3621
yu7400ki <yu7400ki@gmail>
2022-09-22 16:39:27 +0900

    create empty pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Create } from "./pages/Create/Create";
+import { Explore } from "./pages/Explore/Explore";
+import { Login } from "./pages/Login/Login";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/create/" element={<Create />}/>
+        <Route path="/explore/" element={<Explore />}/>
+        <Route path="/login/" element={<Login />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
